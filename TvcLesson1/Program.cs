277048a1258c4fa2546200ccd2a96a52807c4d3f
unknown <hungcungg312@gmail.com>
2026-08-21 16:14:14 +0700

    TvcLesson1 - First WebApp

---
+++ TvcLesson1/Program.cs
@@ -0,0 +1,15 @@
+namespace TvcLesson1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello Nguyễn Tới!");
+
+            app.Run();
+        }
+    }
+}
